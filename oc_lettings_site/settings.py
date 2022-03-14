d25ae5b574dc39507c39a183691b7be5aeb49560
Thomas2020 <thomas.duval2020@campus-eni.fr>
2022-03-14 20:38:46 +0100

    DEBUG

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", default="default")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", default="True")
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ["oc-lettings-thomas.herokuapp.com"]
 
