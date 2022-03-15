90fbe7194c22c13b23e662c014fce0403b12d4f4
Thomas2020 <thomas.duval2020@campus-eni.fr>
2022-03-15 12:33:40 +0100

    final

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 from . import views
 
@@ -16,4 +15,4 @@ urlpatterns = [
     path("profiles/", include("profiles.urls"), name="profiles"),
     path("admin/", admin.site.urls),
     path("sentry-debug/", trigger_error),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
