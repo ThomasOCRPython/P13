85edef1d6952d0c1ec678d4aa1ef05b4d3a79680
Thomas2020 <thomas.duval2020@campus-eni.fr>
2022-03-15 11:32:39 +0100

    test

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -14,4 +16,4 @@ urlpatterns = [
     path("profiles/", include("profiles.urls"), name="profiles"),
     path("admin/", admin.site.urls),
     path("sentry-debug/", trigger_error),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
